6c1eaa9850ac45986fa44b8345d34372ccee0b1b
Tomás Messina <45271713+messina8@users.noreply.github.com>
2022-08-25 22:30:59 -0300

    Finished Models, migrated and instantiated Databases, added models to admin.py files.

--- Biblioteca/admin.py
+++ Biblioteca/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+import Biblioteca.models
+
+
 # Register your models here.
+admin.site.register(Biblioteca.models.Book)
+admin.site.register(Biblioteca.models.Clients)
+admin.site.register(Biblioteca.models.Invoices)
+admin.site.register(Biblioteca.models.ToDoItem)
+admin.site.register(Biblioteca.models.TimeTable)
+admin.site.register(Biblioteca.models.OwnedBooks)
+admin.site.register(Biblioteca.models.JournalEntry)# Shouldn't be accessible but is,for the sake of testing
--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+import login.models
 
 # Register your models here.
+
+admin.site.register(login.models.LogHistory)  # Shouldn't be accessible but is,for the sake of testing

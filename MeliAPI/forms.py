6a2fb8a57c031272346a0bf83b241fc666e4bc4e
Tomás Messina <45271713+messina8@users.noreply.github.com>
2022-09-17 09:18:12 -0300

    updated Price Checking view.

--- MeliAPI/forms.py
+++ MeliAPI/forms.py
@@ -4,3 +4,4 @@ from django import forms
 
 class PriceCheckForm(forms.Form):
     product_name = forms.CharField(label='Product Name:', max_length=40)
+    used_search = forms.BooleanField(label='Search Used', widget=forms.CheckboxInput, required=False)
--- MeliAPI/views.py
+++ MeliAPI/views.py
@@ -12,31 +12,34 @@ def prices(request):
         if search_form.is_valid():
 
             search = search_form.cleaned_data['product_name']
+            used = search_form.cleaned_data['used_search']
+
+            price_dict = {}
 
             official_stores_response = apirequests.get(
                 'https://api.mercadolibre.com/sites/MLA/search?category=MLA3025&official_store'
                 '=all&q=' + urllib.parse.quote(search))
-            used_response = apirequests.get('https://api.mercadolibre.com/sites/MLA/search?category=MLA3025'
-                                            '&condition'
-                                            '=used&q=' + urllib.parse.quote(search))
-            price_dict = {
-                'used': [],
-                'official_stores': []
-            }
-            official_stores_results = official_stores_response.json()
-            for element in official_stores_results['results']:
-                seller_name = urllib.parse.unquote(str(element['seller']['permalink'])).split('/')[3].replace('+', ' ')
-                price_dict['official_stores'].append(
-                    {"title": element['title'], 'price': element['price'], 'seller': seller_name,
-                     'image': element['thumbnail']})
-            used_results = used_response.json()
-            for element in used_results['results']:
-                price_dict['used'].append(
-                    {'title': element['title'], 'price': element['price'], 'image': element['thumbnail']})
+            if used:
+                used_response = apirequests.get('https://api.mercadolibre.com/sites/MLA/search?category=MLA3025'
+                                                '&condition'
+                                                '=used&q=' + urllib.parse.quote(search))
+                price_dict['used'] = data_organizer(used_response.json())
 
-            context = {'message': search, 'results': price_dict, 'form': form, 'hide': True}
+            price_dict['official_stores'] = data_organizer(official_stores_response.json())
+
+            context = {'message': search, 'results': price_dict, 'form': form, 'used': used}
             return render(request, 'MeliAPI/prices.html', context)
 
     else:
         context = {'message': 'Price search', 'form': form, 'hide': True}
         return render(request, 'MeliAPI/prices.html', context)
+
+
+def data_organizer(response):
+    data = []
+    for element in response['results']:
+        seller_name = urllib.parse.unquote(str(element['seller']['permalink'])).split('/')[3].replace('+', ' ')
+
+        data.append({"title": element['title'], 'price': element['price'], 'seller': seller_name,
+                     'image': element['thumbnail']})
+    return data
